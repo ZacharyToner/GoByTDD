85a56dea73223e1d11ace06d5203b157a8f44c03
Zachary Toner <zack.toner@me.com>
2018-12-18 16:28:03 -0800

    Using range instead

--- arrays/sum.go
+++ arrays/sum.go
@@ -4,8 +4,8 @@ package arrays
 //Sum returns the integer value from adding everything in the provided array
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
