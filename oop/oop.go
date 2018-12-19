d862d64c26668c4c8104e5e55ccf5ca8a1ec57ad
Zachary Toner <zack.toner@me.com>
2018-12-18 17:15:53 -0800

    Basic Perimeter

---
+++ oop/oop.go
@@ -0,0 +1,7 @@
+//Package oop is for exploring structs, methods, and interfaces
+package oop
+
+//Perimeter returns the perimeter based on the provided width and height
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
