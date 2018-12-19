ddfbd5a3ec7b510aaa0e193a750d0233efd5e142
Zachary Toner <zack.toner@me.com>
2018-12-19 15:36:45 -0800

    OOP Chapter Complete

--- oop/oop.go
+++ oop/oop.go
@@ -1,7 +1,46 @@
 //Package oop is for exploring structs, methods, and interfaces
 package oop
 
-//Perimeter returns the perimeter based on the provided width and height
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+//Shape is our base interface for all shapes
+type Shape interface {
+	Area() float64
+}
+
+//Rectangle is our custom type to represent rectangles
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+//Area returns the area of our Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+//Circle is our custom type to represent circles
+type Circle struct {
+	Radius float64
+}
+
+//Area returns the area of our circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+//Triangle is our custom type to represent triangles
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+//Area returns the area of our triangle
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+//Perimeter returns the perimeter of a Rectangle
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.Width + rectangle.Height) * 2
 }
