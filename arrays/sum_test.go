1e1904adadfc795f944abccf65d0505440614f94
Zachary Toner <zack.toner@me.com>
2018-12-18 16:45:03 -0800

    Refactoring to only one needed test and using slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,15 +3,21 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
+	numbers := []int{1, 2, 3}
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	got := Sum(numbers)
+	want := 6
 
-		got := Sum(numbers)
-		want := 6
+	if want != got {
+		t.Errorf("get %d but wanted %d, given %v", got, want, numbers)
+	}
+}
 
-		if want != got {
-			t.Errorf("get %d but wanted %d, given %v", got, want, numbers)
-		}
-	})
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 3}, []int{7, 8, 9}, []int{0, 9})
+	want := []int{6, 24, 9}
+
+	if want != got {
+		t.Errorf("get %v but wanted %v", got, want)
+	}
 }
