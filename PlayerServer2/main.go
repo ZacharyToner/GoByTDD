4f2976529a819ff387bda4985280ba9708060140
Zack Toner <ztoner@apple.com>
2019-01-02 15:35:16 -0800

    In Memory Store Complete

---
+++ PlayerServer2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	handler := http.HandlerFunc(server.ServeHttp)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
